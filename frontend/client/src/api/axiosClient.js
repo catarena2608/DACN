fe04e9c46c12a57c427b416596c24bb98124b935
catarena2608 <thodeptrai11111@gmail.com>
2026-05-08 02:45:23 +0700

    frontend

--- frontend/client/src/api/axiosClient.js
+++ frontend/client/src/api/axiosClient.js
@@ -43,7 +43,7 @@ axiosClient.interceptors.response.use(
 
         originalRequest.headers.Authorization = `Bearer ${res.data.accessToken}`;
         return axiosClient(originalRequest);
-      } catch (e) {
+      } catch (_error) {
         store.dispatch(logout());
       }
     }
