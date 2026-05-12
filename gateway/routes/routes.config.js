7c33732beb556695c7cacc4544b2a0ea5d17cea4
catarena2608 <thodeptrai11111@gmail.com>
2026-05-13 03:58:09 +0700

    first initial commit

--- gateway/routes/routes.config.js
+++ gateway/routes/routes.config.js
@@ -1,6 +1,7 @@
 // routes/routes.config.js
 module.exports = {
   "/api/auth": process.env.AUTH_SERVICE_URL,  // Auth service
+  "/api/products": process.env.PRODUCT_SERVICE_URL,
   "/api/users": process.env.PRODUCT_SERVICE_URL, // User service
   "/api/follow": process.env.PRODUCT_SERVICE_URL,
 };
