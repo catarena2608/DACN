7fbbcdd25b445bf2c40d4788978c020144e5f639
catarena2608 <thodeptrai11111@gmail.com>
2026-05-13 08:07:10 +0700

    first initial commit

--- backend-auth/src/app.js
+++ backend-auth/src/app.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth/health", healthRoutes);
+app.use("/health", healthRoutes);
 app.use("/api/auth", authRoutes);
 
 // ================== MONGOOSE CONNECT ==================
