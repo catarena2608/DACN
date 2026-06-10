8cebe05429b15b9e161cc8d5b8cf844175173abb
catarena2608 <thodeptrai11111@gmail.com>
2026-06-10 20:55:05 +0700

    fix conflict

--- services/backend-auth/src/utils/jwt.js
+++ services/backend-auth/src/utils/jwt.js
@@ -1,14 +1,17 @@
 const jwt = require("jsonwebtoken");
+const { randomUUID } = require("crypto");
 
 exports.signAccessToken = (payload) => {
   return jwt.sign(payload, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
+    jwtid: randomUUID(),
   });
 };
 
 exports.signRefreshToken = (payload) => {
   return jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
     expiresIn: process.env.JWT_REFRESH_EXPIRES_IN,
+    jwtid: randomUUID(),
   });
 };
 
